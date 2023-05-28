41c1d1aaaf33d46a147fb792dd9aba69374106a4
ahmad raja <ahmad.raja@stackfusion.io>
2023-05-28 16:54:00 +0530

    configured with cores header and updated requirement.txt

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'Account',
     "rest_framework_simplejwt",
+    "corsheaders",
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -140,6 +142,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (

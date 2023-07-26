488b07276101e70592cacfbaa6862092f558a971
Ahmad <ahmadraza@001912@gmail.com>
2023-07-26 11:09:17 +0530

    Added link to the url

--- ecommerce/Homeapp/urls.py
+++ ecommerce/Homeapp/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns = [
-  path("",views.Home,name="home"),
-  path("shop",views.Shop,name="shop"),
-  path("test",views.Test,name="test"),
+  path("",views.Home_view,name="home"),
+  path("shop",views.Shop_view,name="shop"),
+  path('detail/', views.detail_view, name='detail'),
+  path('cart/', views.cart_view, name='cart'),
+  path('checkout/', views.checkout_view, name='checkout'),
+  path('contact/', views.contact_view, name='contact'),
 ]
 

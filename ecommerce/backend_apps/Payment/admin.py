8ba2de159c4cca42f7953af07ea984fa4dc9e8d6
Ahmad <ahmadraza@001912@gmail.com>
2023-08-13 22:18:55 +0530

    Added Payment app

--- ecommerce/backend_apps/Payment/admin.py
+++ ecommerce/backend_apps/Payment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import PaymentType, UserPaymentMethod
 
 # Register your models here.
+
+@admin.register(PaymentType)
+class PaymentTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    list_per_page = 20
+
+@admin.register(UserPaymentMethod)
+class UserPaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_type', 'payment_info', 'is_default')
+    list_filter = ('user', 'payment_type', 'is_default')
+    search_fields = ('user__username', 'payment_type__name', 'payment_info')
+    list_per_page = 20
--- ecommerce/backend_apps/Payment/models.py
+++ ecommerce/backend_apps/Payment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from Account.models import User
 # Create your models here.
+
+
+
+class PaymentType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class UserPaymentMethod(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT)
+    payment_info = models.CharField(max_length=100)  # Store payment details like card number, UPI ID, etc.
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s {self.payment_type.name} Payment"

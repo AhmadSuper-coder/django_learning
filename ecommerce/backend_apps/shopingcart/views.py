bb2d6ab892c2759ed034e546c1fc10edaeced6ef
Ahmad <ahmadraza@001912@gmail.com>
2023-10-15 00:15:54 +0530

    Added views in shoping cart

--- ecommerce/backend_apps/shopingcart/views.py
+++ ecommerce/backend_apps/shopingcart/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from backend_apps.shopingcart.models import CartItem
 # Create your views here.
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [IsAuthenticated,]
+    def get(self,request,format=None):
+        cart_data=CartItem.objects.get()
+        # return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
